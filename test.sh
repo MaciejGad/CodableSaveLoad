9b5b8bbb61e3ba852b2b8372f978ef3ef85cf6ab
Maciej Gad <gad.maciej@gmail.com>
2019-07-24 13:47:51 +0200

    move code coverage to docs folder

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xcodebuild test -scheme CodableSaveLoad -workspace CodableSaveLoad.xcworkspace -destination 'platform=iOS Simulator,name=iPhone SE'  | xcpretty
-slather coverage -s --scheme CodableSaveLoad --workspace CodableSaveLoad.xcworkspace CodableSaveLoad.xcodeproj
+slather coverage --html --show --scheme CodableSaveLoad --workspace CodableSaveLoad.xcworkspace --output-directory docs CodableSaveLoad.xcodeproj
