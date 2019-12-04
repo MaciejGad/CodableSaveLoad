4838c5ca05bbb08635207133725ca1b534a71853
Maciej Gad <gad.maciej@gmail.com>
2019-12-04 16:18:58 +0100

    Update to swift 5.1 and Xcode 11.2

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 swift test
-xcodebuild test -scheme CodableSaveLoad -destination 'platform=iOS Simulator,name=iPhone SE'  | xcpretty
+xcodebuild test -scheme CodableSaveLoad -destination 'platform=iOS Simulator,name=iPhone 8'  | xcpretty
 bundler exec slather coverage --html --show --scheme CodableSaveLoad --output-directory docs CodableSaveLoad.xcodeproj
